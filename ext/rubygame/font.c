4125f9c09b182dcfa6e34a99ae15640d1d9be341
John Croisant <jacius@gmail.com>
2005-05-08 23:24:14 +0000

    - Move Font.quit, .version, .usable? to TTF class-methods of the same name. - Move Font.init to TTF.setup class-method. - Change TTF#bold=, #italic=, and u#nderline= to return the old value. - Added aliases: TTF#b, #b=, #i, #i=, #u, and #u= for bold, italic, and   underline (getters and setters). - First attempt at documentation comments in RDoc format.

--- ext/rubygame/font.c
+++ ext/rubygame/font.c
@@ -22,8 +22,16 @@
 #ifdef HAVE_SDL_TTF_H
 #include "SDL_ttf.h"
 
-/* Return the major, minor, and patch numbers for SDL_ttf */
-VALUE rbgm_font_version(VALUE module)
+/*
+ *  call-seq:
+ *     Rubygame::TTF.version  =>  [major,minor,patch]
+ *
+ *  Return the major, minor, and patch numbers for the version of SDL_ttf 
+ *  that Rubygame was compiled with. If Rubygame was not compiled with SDL_ttf,
+ *  all version numbers will be +0+ (zero), and you should not attempt to use
+ *  the TTF class.
+ */
+VALUE rbgm_ttf_version(VALUE module)
 { 
   return rb_ary_new3(3,
 					 INT2NUM(SDL_TTF_MAJOR_VERSION),
@@ -31,20 +39,53 @@ VALUE rbgm_font_version(VALUE module)
 					 INT2NUM(SDL_TTF_PATCHLEVEL));
 }
 
-VALUE rbgm_font_init(VALUE module)
+/*
+ *  call-seq:
+ *     Rubygame::TTF.setup  =>  nil
+ *
+ *  Attempt to setup the TTF class for use by initializing SDL_ttf.
+ *  This _must_ be called before the TTF class can be used.
+ *  Raises SDLError if there is a problem initializing SDL_ttf.
+ */
+VALUE rbgm_ttf_setup(VALUE module)
 {
 	if(TTF_Init()!=0)
-		rb_raise(eSDLError,"could not initialize Font module: %s",TTF_GetError());
+		rb_raise(eSDLError,"could not setup TTF class: %s",TTF_GetError());
 	return Qnil;
 }
 
-VALUE rbgm_font_quit(VALUE module)
+/*
+ *  call-seq:
+ *     Rubygame::TTF.quit  =>  nil
+ *
+ *  Clean up and quit SDL_ttf, making the TTF class unusable as a result
+ *  (until it is setup again). This does not need to be called before Rubygame
+ *  exits, as it will be done automatically.
+ */
+VALUE rbgm_ttf_quit(VALUE module)
 {
 	if(TTF_WasInit())
 		TTF_Quit();
 	return Qnil;
 }
 
+/*
+ *  call-seq:
+ *     Rubygame::TTF.new( file, size )  =>  TTF
+ *
+ *  Create a new TTF object, which can render text to a Surface with a
+ *  particular font style and size.
+ *
+ *  This function takes these arguments:
+ *  - file:: filename of the TrueType font to use. Should be a +.TTF+ or 
+ *           +.FON+ file.
+ *  - size:: point size (based on 72DPI). Or, the height in pixels from the
+ *           bottom of the descent to the top of the ascent.
+ *
+ *           Please note that pygame scales fonts by a factor of 0.6875, so to
+ *           replicate the apparent font size of a pygame application, you will
+ *           need to provide an equally scaled font size to Rubygame.
+ */
 VALUE rbgm_ttf_new(int argc, VALUE *argv, VALUE class)
 {
 	VALUE self;
@@ -66,6 +107,11 @@ VALUE rbgm_ttf_initialize(int argc, VALUE *argv, VALUE self)
 	return self;
 }
 
+/*  call-seq:
+ *     TTF#bold  =>  value
+ *
+ *  True if bolding is enabled for the font.
+ */
 VALUE rbgm_ttf_getbold(VALUE self)
 {
 	TTF_Font *font;
@@ -79,6 +125,11 @@ VALUE rbgm_ttf_getbold(VALUE self)
 		return Qfalse;
 }
 
+/*  call-seq:
+ *     TTF#bold = value  =>  old_value
+ *
+ *  Set whether bolding is enabled for this font. Returns the old value.
+ */
 VALUE rbgm_ttf_setbold(VALUE self,VALUE bold)
 {
 	TTF_Font *font;
@@ -86,13 +137,28 @@ VALUE rbgm_ttf_setbold(VALUE self,VALUE bold)
 
 	Data_Get_Struct(self,TTF_Font,font);
 	style = TTF_GetFontStyle(font);
+
+	/* Font is currently bold, and we want it to be not bold. */
 	if((style & TTF_STYLE_BOLD) == TTF_STYLE_BOLD && !bold)
+	  {
 		TTF_SetFontStyle(font,style^TTF_STYLE_BOLD);
+		return Qtrue;			/* The old value */
+	  }
+	/* Font is not currently bold, and we want it to be bold. */
 	else if(bold)
+	  {
 		TTF_SetFontStyle(font,style|TTF_STYLE_BOLD);
-	return self;
+		return Qfalse;			/* The old value */
+	  }
+	/* No changes were necessary. */
+	return bold;				/* Same as old value */
 }
 
+/*  call-seq:
+ *     TTF#italic  =>  value
+ *
+ *  True if italicizing is enabled for the font.
+ */
 VALUE rbgm_ttf_getitalic(VALUE self)
 {
 	TTF_Font *font;
@@ -106,6 +172,11 @@ VALUE rbgm_ttf_getitalic(VALUE self)
 		return Qfalse;
 }
 
+/*  call-seq:
+ *     TTF#italic = value  =>  old_value
+ *
+ *  Set whether italicizing is enabled for this font. Returns the old value.
+ */
 VALUE rbgm_ttf_setitalic(VALUE self,VALUE italic)
 {
 	TTF_Font *font;
@@ -113,13 +184,28 @@ VALUE rbgm_ttf_setitalic(VALUE self,VALUE italic)
 
 	Data_Get_Struct(self,TTF_Font,font);
 	style = TTF_GetFontStyle(font);
+
+	/* Font is currently italic, and we want it to be not italic. */
 	if((style & TTF_STYLE_ITALIC) == TTF_STYLE_ITALIC && !italic)
+	  {
 		TTF_SetFontStyle(font,style^TTF_STYLE_ITALIC);
+		return Qtrue;			/* The old value */
+	  }
+	/* Font is not currently italic, and we want it to be italic. */
 	else if(italic)
+	  {
 		TTF_SetFontStyle(font,style|TTF_STYLE_ITALIC);
-	return self;
+		return Qfalse;			/* The old value */
+	  }
+	/* No changes were necessary. */
+	return italic;				/* Same as old value */
 }
 
+/*  call-seq:
+ *     TTF#underline  =>  value
+ *
+ *  True if underlining is enabled for the font.
+ */
 VALUE rbgm_ttf_getunderline(VALUE self)
 {
 	TTF_Font *font;
@@ -133,6 +219,11 @@ VALUE rbgm_ttf_getunderline(VALUE self)
 		return Qfalse;
 }
 
+/*  call-seq:
+ *     TTF#underline = value  =>  old_value
+ *
+ *  Set whether underlining is enabled for this font. Returns the old value.
+ */
 VALUE rbgm_ttf_setunderline(VALUE self,VALUE underline)
 {
 	TTF_Font *font;
@@ -140,13 +231,29 @@ VALUE rbgm_ttf_setunderline(VALUE self,VALUE underline)
 
 	Data_Get_Struct(self,TTF_Font,font);
 	style = TTF_GetFontStyle(font);
+
+	/* Font is currently underlined, and we want it to be not underlined. */
 	if((style & TTF_STYLE_UNDERLINE) == TTF_STYLE_UNDERLINE && !underline)
+	  {
 		TTF_SetFontStyle(font,style^TTF_STYLE_UNDERLINE);
+		return Qtrue;			/* The old value */
+	  }
+	/* Font is not currently underlined, and we want it to be underlined. */
 	else if(underline)
+	  {
 		TTF_SetFontStyle(font,style|TTF_STYLE_UNDERLINE);
-	return self;
+		return Qfalse;			/* The old value */
+	  }
+	/* No changes were necessary. */
+	return underline;			/* Same as old value */
 }
 
+/*  call-seq:
+ *     TTF#height  =>  value
+ *
+ *  Return the biggest height (bottom to top; in pixels) of all glyphs in the 
+ *  font.
+ */
 VALUE rbgm_ttf_height(VALUE self)
 {
 	TTF_Font *font;
@@ -154,6 +261,12 @@ VALUE rbgm_ttf_height(VALUE self)
 	return INT2NUM(TTF_FontHeight(font));
 }
 
+/*  call-seq:
+ *     TTF#ascent  =>  value
+ *
+ *  Return the biggest ascent (baseline to top; in pixels) of all glyphs in 
+ *  the font.
+ */
 VALUE rbgm_ttf_ascent(VALUE self)
 {
 	TTF_Font *font;
@@ -161,6 +274,12 @@ VALUE rbgm_ttf_ascent(VALUE self)
 	return INT2NUM(TTF_FontAscent(font));
 }
 
+/*  call-seq:
+ *     TTF#descent  =>  value
+ *
+ *  Return the biggest descent (baseline to bottom; in pixels) of all glyphs in
+ *  the font.
+ */
 VALUE rbgm_ttf_descent(VALUE self)
 {
 	TTF_Font *font;
@@ -168,6 +287,12 @@ VALUE rbgm_ttf_descent(VALUE self)
 	return INT2NUM(TTF_FontDescent(font));
 }
 
+/*  call-seq:
+ *     TTF#lineskip  =>  value
+ *
+ *  Return the recommended distance from a point on a line of text to the
+ *  same point on the line of text below it.
+ */
 VALUE rbgm_ttf_lineskip(VALUE self)
 {
 	TTF_Font *font;
@@ -175,6 +300,19 @@ VALUE rbgm_ttf_lineskip(VALUE self)
 	return INT2NUM(TTF_FontLineSkip(font));
 }
 
+/*  call-seq:
+ *     TTF#render(string, aa, fg, bg)  =>  Surface
+ *
+ *  Renders a string to a Surface with the font's style and the given color(s).
+ *
+ *  This method takes these arguments:
+ *  - string:: the text string to render
+ *  - aa::     whether or not to anti-aliasing. Enabling this makes the text
+ *             look much nicer (smooth curves), but is much slower.
+ *  - fg::     the color to render the text as, in the form +[r,g,b]+
+ *  - bg::     the color to use as a background for the text. This option can
+ *             be omitted to have a transparent background.
+ */
 VALUE rbgm_ttf_render(int argc, VALUE *argv, VALUE self)
 {
 	SDL_Surface *surf;
@@ -230,26 +368,35 @@ VALUE rbgm_ttf_render(int argc, VALUE *argv, VALUE self)
 void Rubygame_Init_Font()
 {
 	mFont = rb_define_module_under(mRubygame,"Font");
-	rb_define_module_function(mFont,"usable?",rbgm_usable,0);
-	rb_define_module_function(mFont,"version",rbgm_font_version,0);
-	rb_define_module_function(mFont,"init",rbgm_font_init,0);
-	rb_define_module_function(mFont,"quit",rbgm_font_quit,0);
 
 	cTTF = rb_define_class_under(mFont,"TTF",rb_cObject);
 	rb_define_singleton_method(cTTF,"new",rbgm_ttf_new,-1);
+	rb_define_singleton_method(cTTF,"usable?",rbgm_usable,0);
+	rb_define_singleton_method(cTTF,"version",rbgm_font_version,0);
+	rb_define_singleton_method(cTTF,"init",rbgm_font_init,0);
+	rb_define_singleton_method(cTTF,"quit",rbgm_font_quit,0);
+
 	rb_define_method(cTTF,"initialize",rbgm_ttf_initialize,-1);
-	rb_define_method(cTTF,"bold",rbgm_ttf_getbold,0);
-	rb_define_method(cTTF,"bold=",rbgm_ttf_setbold,1);
+	rb_define_method(cTTF,"bold",rbgm_ttf_getitalic,0);
+	rb_define_method(cTTF,"bold=",rbgm_ttf_setitalic,1);
+	rb_define_alias( cTTF,"b","bold")
+	rb_define_alias( cTTF,"b=","bold=")
 	rb_define_method(cTTF,"italic",rbgm_ttf_getitalic,0);
 	rb_define_method(cTTF,"italic=",rbgm_ttf_setitalic,1);
+	rb_define_alias( cTTF,"i","italic")
+	rb_define_alias( cTTF,"i=","italic=")
 	rb_define_method(cTTF,"underline",rbgm_ttf_getunderline,0);
 	rb_define_method(cTTF,"underline=",rbgm_ttf_setunderline,1);
+	rb_define_alias( cTTF,"u","underline")
+	rb_define_alias( cTTF,"u=","underline=")
 	rb_define_method(cTTF,"height",rbgm_ttf_height,0);
 	rb_define_method(cTTF,"ascent",rbgm_ttf_ascent,0);
 	rb_define_method(cTTF,"descent",rbgm_ttf_descent,0);
 	rb_define_method(cTTF,"line_skip",rbgm_ttf_lineskip,0);
 	rb_define_method(cTTF,"render",rbgm_ttf_render,-1);
 }
+
+/* -- */
 
 #else /* HAVE_SDL_TTF_H */
 
@@ -265,13 +412,14 @@ VALUE rbgm_font_version(VALUE module)
 void Rubygame_Init_Font()
 {
 	mFont = rb_define_module_under(mRubygame,"Font");
-	rb_define_module_function(mFont,"usable?",rbgm_unusable,0);
-	rb_define_module_function(mFont,"version",rbgm_font_version,0);
-	rb_define_module_function(mFont,"init",rbgm_dummy,-1);
-	rb_define_module_function(mFont,"quit",rbgm_dummy,-1);
 
 	cTTF = rb_define_class_under(mFont,"TTF",rb_cObject);
 	rb_define_singleton_method(cTTF,"new",rbgm_dummy,-1);
+	rb_define_singleton_method(cTTF,"usable?",rbgm_unusable,0);
+	rb_define_singleton_method(cTTF,"version",rbgm_font_version,0);
+	rb_define_singleton_method(cTTF,"setup",rbgm_dummy,-1);
+	rb_define_singleton_method(cTTF,"quit",rbgm_dummy,-1);
+
 	rb_define_method(cTTF,"initialize",rbgm_dummy,-1);
 	/* No TTF objects can be made, so the other funcs are unneeded. Maybe. */
 }
