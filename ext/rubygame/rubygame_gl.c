8c7db3482aab7e2ad06fdecd3bda86e791f48911
John Croisant <jacius@gmail.com>
2006-01-21 18:08:39 +0000

    - gl.c: GL module and methods fully documented.

--- ext/rubygame/rubygame_gl.c
+++ ext/rubygame/rubygame_gl.c
@@ -32,6 +32,16 @@
 
 #ifdef HAVE_OPENGL
 
+/*  call-seq:
+ *    get_attrib( attrib )  ->  Integer
+ *
+ *  Return the value of the the SDL/OpenGL attribute identified by +attrib+,
+ *  which should be one of the constants defined in the Rubygame::GL module.
+ *  See #set_attrib for a list of attribute constants.
+ *
+ *  This method is useful after using #set_attrib and calling Screen#set_mode,
+ *  to make sure the attribute is the expected value.
+ */
 VALUE rbgm_gl_getattrib(VALUE module, VALUE attr)
 {
   int val;
@@ -40,6 +50,31 @@ VALUE rbgm_gl_getattrib(VALUE module, VALUE attr)
   return INT2NUM(val);
 }
 
+/*  call-seq:
+ *    set_attrib( attrib, value )  ->  nil
+ *
+ *  Set the SDL/OpenGL attribute +attrib+ to +value+. This should be called
+ *  *before* you call Screen#set_mode with the OPENGL flag. You may wish to
+ *  use #get_attrib after calling Screen#set_mode to confirm that the attribute
+ *  is set to the desired value.
+ *
+ *  The full list of SDL/OpenGL attribute identifier constants (located under
+ *  the Rubygame::GL module) is as follows:
+ *
+ *  RED_SIZE::         Size of framebuffer red component, in bits.
+ *  GREEN_SIZE::       Size of framebuffer green component, in bits.
+ *  BLUE_SIZE::        Size of framebuffer blue component, in bits.
+ *  ALPHA_SIZE::       Size of framebuffer alpha (opacity) component, in bits.
+ *  BUFFER_SIZE::      Size of framebuffer, in bits.
+ *  DOUBLEBUFFER::     Enable or disable double-buffering.
+ *  DEPTH_SIZE::       Size of depth buffer, in bits.
+ *  STENCIL_SIZE::     Size of stencil buffer, in bits.
+ *  ACCUM_RED_SIZE::   Size of accumulation buffer red component, in bits.
+ *  ACCUM_GREEN_SIZE:: Size of accumulation buffer green component, in bits.
+ *  ACCUM_BLUE_SIZE::  Size of accumulation buffer blue component, in bits.
+ *  ACCUM_ALPHA_SIZE:: Size of accumulation buffer alpha component, in bits.
+ *  
+ */
 VALUE rbgm_gl_setattrib(VALUE module,VALUE attr,VALUE val)
 {
   if(SDL_GL_SetAttribute(NUM2INT(attr),NUM2INT(val))==-1)
@@ -47,6 +82,13 @@ VALUE rbgm_gl_setattrib(VALUE module,VALUE attr,VALUE val)
   return Qnil;
 }
 
+/*  call-seq:
+ *    swap_buffers( )  ->  nil
+ *
+ *  Swap the back and front buffers, for double-buffered OpenGL displays.
+ *  Should be safe to use (albeit with no effect) on single-buffered OpenGL 
+ *  displays.
+ */
 VALUE rbgm_gl_swapbuffers(VALUE module)
 {
   SDL_GL_SwapBuffers();
@@ -55,8 +97,28 @@ VALUE rbgm_gl_swapbuffers(VALUE module)
 
 #endif
 
+/*  Document-module: Rubygame::GL
+ *
+ *  The GL module provides an interface to SDL's OpenGL-related functions,
+ *  allowing a Rubygame application to create hardware-accelerated 3D graphics
+ *  with OpenGL.
+ *
+ *  Please note that Rubygame itself does not provide an interface to OpenGL
+ *  functions -- only functions which allow Rubygame to work together with
+ *  OpenGL. You will need to use another library, for example 
+ *  ruby-opengl[http://www2.giganet.net/~yoshi/], to actually create graphics
+ *  with OpenGL.
+ *
+ *  Users who wish to use Rubygame Surfaces as textures in OpenGL will want
+ *  to see also the Surface#pixels method.
+ */
 void Rubygame_Init_GL()
 {
+#if 0
+	/* Pretend to define Rubygame module, so RDoc knows about it: */
+	mRubygame = rb_define_module("Rubygame");
+#endif
+
   mGL = rb_define_module_under(mRubygame,"GL");
   Define_GL_Constants();
 
