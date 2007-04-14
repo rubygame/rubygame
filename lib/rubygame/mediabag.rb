4cae576322c11a7c7ac86b74e9c15529764c5bad
John Croisant <jacius@gmail.com>
2007-04-14 02:10:20 +0000

    Updated to Surface.load_image. Supports sound files now too.

--- lib/rubygame/mediabag.rb
+++ lib/rubygame/mediabag.rb
@@ -1,6 +1,6 @@
 #--
 #	Rubygame -- Ruby bindings to SDL to facilitate game creation
-#	Copyright (C) 2004-2006  John 'jacius' Croisant
+#	Copyright (C) 2004-2007  John Croisant
 #
 #	This library is free software; you can redistribute it and/or
 #	modify it under the terms of the GNU Lesser General Public
@@ -21,21 +21,25 @@ require "rubygame"
 
 module Rubygame
 
-  # A Hash-like class which will load and retain media files (currently only
-  # images), so that the file can be loaded once, but used many times.
+  # A Hash-like class which will load and retain media files (images and
+  # sounds), so that the file can be loaded once, but used many times.
   # 
   # The first time a file is requested with the #[] method,that file will be
   # loaded into memory. All subsequent requests for the same file will return
   # a reference to the already-loaded version. Ideally, objects should not
   # have to know whether or not the image has been loaded or not.
   class MediaBag
+    @@image_ext = %W{bmp gif jpg lbm pcx png pnm ppm pgm pbm tga tif xcf xpm}
+    @@sound_ext = %W{wav}
+
     def initialize()
       @media = Hash.new
     end
 
-    # Return a reference to the stored value for key. If there is no value for
-    # key, automatically attempt to load key as an image file name, and store the
-    # loaded Surface as key.
+    # Return a reference to the stored value for key. 
+    # If there is no value for key, automatically attempt to load key
+    # as a filename (guessing the file type based on its extension)
+    # 
     def [](key)
       @media[key] or load(key)
     rescue Rubygame::SDLError
@@ -44,7 +48,7 @@ module Rubygame
 
     # Load the file, but only if it has not been previously loaded.
     def load(filename)
-      store(filename,Rubygame::Image.load(filename))
+      @media[filename] or store( filename, load_file(filename) )
     end
 
     # Store value as key, but only if there is no previous value.
@@ -55,12 +59,32 @@ module Rubygame
     # Forcibly (re)load the file, replacing the previous version in memory 
     # (if any).
     def force_load(filename)
-      force_store(filename,Rubygame::Image.load(filename))
+      force_store( filename, load_file(filename) )
     end
 
     # Forcibly store value as key, replacing the previous value (if any).
     def force_store(key,value)
       @media[key] = value
+    end
+
+    def load_file(filename)
+      case File::extname(filename).downcase[1..-1]
+      when *(@@image_ext)
+        return load_image(filename)
+      when *(@@sound_ext)
+        return load_sound(filename)
+      else
+        raise(ArgumentError,"Unrecognized file extension `%s': %s"%
+              [File::extname(filename), filename])
+      end
+    end
+
+    def load_image(filename)
+      return Rubygame::Surface.load_image(filename)
+    end
+
+    def load_sound(filename)
+      return Rubygame::Mixer::Sample.new(filename)
     end
   end
 
