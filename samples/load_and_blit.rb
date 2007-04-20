bcbc70f5252a3ebd44bddc565da3fbda846b7ef9
John Croisant <jacius@gmail.com>
2007-04-20 02:14:01 +0000

    Updated.

--- samples/load_and_blit.rb
+++ samples/load_and_blit.rb
@@ -1,29 +1,14 @@
 #/usr/bin/env ruby
 
 require "rubygame"
+include Rubygame
 
 Rubygame.init
-
-def wait_for_keypress(queue)
-	catch :keypress do
-		loop do
-			queue.get.each do |event|
-				case(event)
-				when Rubygame::QuitEvent
-					throw :keypress
-				when Rubygame::KeyDownEvent
-					throw :keypress
-				end
-			end
-			Rubygame::Time.wait(5)
-		end
-	end
-end
-
-screen = Rubygame::Screen.set_mode([320,240])
-queue = Rubygame::Queue.instance
-image = Rubygame::Image.load("panda.png")
+screen = Screen.set_mode([320,240])
+queue = EventQueue.new() { 
+  |q| q.ignore = [MouseMotionEvent, ActiveEvent]
+}
+image = Surface.load_image("panda.png")
 puts "Size is: [%s,%s]"%image.size
 image.blit(screen,[0,0])
-screen.update()
-wait_for_keypress(queue)
+queue.wait() { screen.update() }
