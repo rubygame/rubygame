5e73c26e91ee59f429f3c733fda4e33d2790a230
John Croisant <jacius@gmail.com>
2007-04-21 04:39:40 +0000

    Updated and improved

--- samples/demo_sfont.rb
+++ samples/demo_sfont.rb
@@ -9,41 +9,43 @@ require "rubygame"
 include Rubygame
 
 def main(*args)
+  font_name = ""
+
 	if args.length < 1
-		puts
-		puts "   Download a SFont-compatible font (there are lots of them at"
-		puts "   http://user.cs.tu-berlin.de/~karlb/sfont/fonts.html) and pass"
-		puts "   it as the first argument, e.g. './demo_sfont.rb my_font.png'"
-		puts
-		puts "   An example SFont-compatible font (term16.png) is included"
-		puts "   in this directory for your convenience."
-		return
+    font_name = "term16.png"
+		puts <<EOF
+You can pass the filename of a SFont-compatible font as the 
+first argument to try it, e.g.: ./demo_sfont.rb my_font.png
+
+There are many sample fonts available online:   
+http://user.cs.tu-berlin.de/~karlb/sfont/fonts.html
+EOF
+  else
+    font_name = args[0]
 	end
 
-	catch :rubygame_quit do
-		screen = Screen.set_mode([700,400])
-		queue = EventQueue.new()
-    queue.ignore = [ActiveEvent,MouseMotionEvent,MouseUpEvent,MouseDownEvent]
+  screen = Screen.set_mode([700,400])
+  queue = EventQueue.new()
+  queue.ignore = [ActiveEvent,MouseMotionEvent,MouseUpEvent,MouseDownEvent]
 
 
-		screen.title = "SFont Test (%s)"%args[0]
-		font = SFont.new(args[0])
-		renders = []
-		renders << font.render("This is: %s"%args[0])
-		renders << font.render("I say, \"I love pie!\"")
-		renders << font.render("I could eat 3.1415926 pies.")
-		renders << font.render("0 1 2 3 4 5 6 7 8 9")
-		renders << font.render("!@\#$%^&*()[]{}<>;:/\\+=-_'`")
-		renders << font.render("abcdefghijklmnopqrstuvwxyz")
-		renders << font.render("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  screen.title = "SFont Test (%s)"%font_name
+  font = SFont.new(font_name)
+  renders = []
+  renders << font.render("This font is: %s"%font_name)
+  renders << font.render("I say, \"I love pie!\"")
+  renders << font.render("I could eat #{Math::PI} pies.")
+  renders << font.render("0 1 2 3 4 5 6 7 8 9")
+  renders << font.render("!@\#$%^&*()[]{}<>;:/\\+=-_'`\"")
+  renders << font.render("abcdefghijklmnopqrstuvwxyz")
+  renders << font.render("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-		(renders.length).times do |i|
-			renders[i].blit(screen,[0,font.height*i])
-		end
-		screen.update()
+  (renders.length).times do |i|
+    renders[i].blit(screen,[10,10+font.height*i])
+  end
+  screen.update()
 
 		queue.wait()
-	end
 end
 
 main(*ARGV)
