8738a77a1ea9f4a055a488fb27d3f25608a92f14
John Croisant <jacius@gmail.com>
2007-04-21 04:46:47 +0000

    Updated/Fussy nitpicking

--- samples/load_and_blit.rb
+++ samples/load_and_blit.rb
@@ -1,14 +1,22 @@
 #/usr/bin/env ruby
 
+# A very basic sample application.
+
 require "rubygame"
 include Rubygame
 
 Rubygame.init
+
 screen = Screen.set_mode([320,240])
+
 queue = EventQueue.new() { 
   |q| q.ignore = [MouseMotionEvent, ActiveEvent]
 }
+
 image = Surface.load_image("panda.png")
 puts "Size is: [%s,%s]"%image.size
 image.blit(screen,[0,0])
+
 queue.wait() { screen.update() }
+
+Rubygame.quit
