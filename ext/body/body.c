ec2fce6455ff1bedcfd9b6eb74e6fe06f057d83a
Rusterholz Stefan <apeiros@gmx.net>
2007-04-27 16:36:55 +0000

    crect branch: added C collision methods (some still stubs)

--- ext/body/body.c
+++ ext/body/body.c
@@ -6,6 +6,8 @@
 #include "rg_cRect.h"
 #include "rg_cCircle.h"
 
+#define MAX_DELTA 0.001
+
 static VALUE mRubygame;
 static VALUE mBody;
 
@@ -30,6 +32,63 @@ int rg_body_collide(*a, *b)
 
 int rg_body_collide_ftor_ftor(rg_ftor *a, rg_ftor *b)
 {
+	return ((fabs(a->x - b->x) < MAX_DELTA) && (fabs(a->x - b->x) < MAX_DELTA))
+}
+
+int rg_body_collide_ftor_segment(rg_ftor *a, rg_segment *b)
+{
+	double x = (a->x - b->start.x)/b->vec.x;
+	return (x >= 0 && x <= 1 && fabs(x - ((a->y - b->start.y)/b->vec.y)) < MAX_DELTA);
+}
+
+int rg_body_collide_ftor_rect(rg_ftor *a, rg_rect *b)
+{
+	// it works by relocating the whole to Origin and then project the
+	// point on the spanning vectors and see if it fits in
+	rg_ftor r;
+	rg_ftor_subtract(&r, a, &b->topleft);
+	double x = rg_ftor_dotproduct(r, &b->horizontal)/rg_ftor_magnitude2(&b->horizontal);
+	double y = rg_ftor_dotproduct(r, &b->vertical)/rg_ftor_magnitude2(&b->vertical);
+	return (0 <= x && x <= 1 && 0 <= y && y <= 1);
+}
+
+int rg_body_collide_ftor_circle(rg_ftor *a, rg_circle *b)
+{
+	rg_ftor r;
+	rg_ftor_subtract(&r, a, &b->center);
+	return ((r.x*r.x+r.y*r.y-b->radius*b->radius) < MAX_DELTA);
+}
+
+int rg_body_collide_segment_segment(rg_segment *a, rg_segment *b)
+{
+	return 0;
+}
+
+int rg_body_collide_segment_rect(rg_segment *a, rg_rect *b)
+{
+	return 0;
+}
+
+int rg_body_collide_segment_circle(rg_segment *a, rg_circle *b)
+{
+	return 0;
+}
+
+int rg_body_collide_rect_rect(rg_rect *a, rg_rect *b)
+{
+	return 0;
+}
+
+int rg_body_collide_rect_circle(rg_rect *a, rg_circle *b)
+{
+	return 0;
+}
+
+int rg_body_collide_circle_circle(rg_circle *a, rg_circle *b)
+{
+	rg_ftor r;
+	rg_ftor_subtract(&r, a->center, b->center);
+	return ((rg_ftor_magnitude(&r)-MAX_DELTA) <= (a->radius+b->radius));
 }
 
 static VALUE rg_body_rb_type(VALUE class, VALUE obj)
