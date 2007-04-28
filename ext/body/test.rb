2a80d2b26966a5ed86d44dc12a4516c9b0e053f4
Rusterholz Stefan <apeiros@gmx.net>
2007-04-28 02:37:50 +0000

    crect branch: test.rb for body is now real unit tests

--- ext/body/test.rb
+++ ext/body/test.rb
@@ -1,27 +1,45 @@
 require 'body'
 include Rubygame::Body
 
-puts "Ftor - Ftor"
-p ["Ftor[20,20].collide?(Ftor[20,20])", Ftor[20,20].collide?(Ftor[20,20])]
-p ["Ftor[20,20].collide?(Ftor[20,0])", Ftor[20,20].collide?(Ftor[20,0])]
-p ["Ftor[0,20].collide?(Ftor[20,20])", Ftor[20,20].collide?(Ftor[0,20])]
-begin
-	p ["Ftor[0,20].collide?(Ftor[20,20])", Ftor[20,20].collide?("foo")]
-rescue => e
-	puts e
-end
-
-puts "\nRect - Ftor"
-p r = Rect.rect(0,0,20,20)
-test = [Ftor[0,30], Ftor[30,0], Ftor[2,2], Ftor[20,20], Ftor[0,20]]
-test.each { |ftor|
-	p [ftor, r.collide?(ftor)]
-}
-p r.collide(test)
-
 require 'test/unit'
 class TestBody < Test::Unit::TestCase
+	def test_raise
+		assert_raises(ArgumentError) { Ftor[20,20].collide?("foo") }
+	end
+
 	def test_ftor_ftor_collision
+		assert(Ftor[20,20].collide?(Ftor[20,20]))
+		assert(!Ftor[20,20].collide?(Ftor[0,0]))
+		assert(!Ftor[20,20].collide?(Ftor[20,0]))
+		assert(!Ftor[20,20].collide?(Ftor[0,20]))
+	end
+	
+	def test_ftor_segment_collision
+		seg = Segment.points(0,0,20,20)
+		collide = [Ftor[0,0], Ftor[10,10], Ftor[20,20]]
+		no_coll = [Ftor[1,0], Ftor[21,21], Ftor[-1,-1]]
+		collide.each { |ftor|
+			assert(seg.collide?(ftor))
+			assert(ftor.collide?(seg))
+		}
+		no_coll.each { |ftor|
+			assert(!seg.collide?(ftor))
+			assert(!ftor.collide?(seg))
+		}
+	end
+	
+	def test_ftor_rect_collision
+		r = Rect.rect(0,0,20,20)
+		collide = [Ftor[11,9], Ftor[20,20], Ftor[20,10], Ftor[0,20]]
+		no_coll = [Ftor[0,30], Ftor[30,0]]
+		collide.each { |ftor|
+			assert(r.collide?(ftor))
+			assert(ftor.collide?(r))
+		}
+		no_coll.each { |ftor|
+			assert(!r.collide?(ftor))
+			assert(!ftor.collide?(r))
+		}
 	end
 end
 
