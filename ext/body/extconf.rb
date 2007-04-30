b696ad8e841bb633aa6ca876d046bfabf5a26c0b
Rusterholz Stefan <apeiros@gmx.net>
2007-04-30 21:30:48 +0000

    Removed old, separate body code

--- ext/body/extconf.rb
+++
@@ -1,5 +0,0 @@
-$preload=nil
-require 'mkmf'
-with_cflags("-W -Wall -std=c99") {
-	create_makefile("body")
-}
--- ext/body/rg_cCircle.h
+++
@@ -1,6 +0,0 @@
-typedef struct rg_circle_struct {
-	rg_ftor center;
-	double radius;
-} rg_circle;
-
-void Init_rg_cCircle();
