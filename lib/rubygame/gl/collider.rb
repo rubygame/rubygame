76fee38fbed94d48129929d16814c680f24d63ae
John Croisant <jacius@gmail.com>
2007-06-17 22:34:17 +0000

    Collider falls back on reverse collide method if available.

--- lib/rubygame/gl/collider.rb
+++ lib/rubygame/gl/collider.rb
@@ -1,6 +1,13 @@
 module Collider
 	def collide( other )
 		collide_method = "collide_#{other.class.name.downcase}".to_sym
-		send( collide_method, other )
+		return send( collide_method, other )
+	rescue NoMethodError => e
+		begin
+			collide_method = "collide_#{self.class.name.downcase}".to_sym
+			return other.send( collide_method, self )
+		rescue NoMethodError
+			raise e
+		end
 	end
 end
