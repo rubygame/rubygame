4a1883cebdf67b836a4779cb2d5f1bc94b276086
John Croisant <jacius@gmail.com>
2007-06-17 22:10:08 +0000

    Simplify, simplify, simplify. (Collider)

--- lib/rubygame/gl/collider.rb
+++ lib/rubygame/gl/collider.rb
@@ -1,13 +1,6 @@
-class NoCollisionHandler < NoMethodError
-end
-
 module Collider
 	def collide( other )
 		collide_method = "collide_#{other.class.name.downcase}".to_sym
-		if respond_to? collide_method
-			return send( collide_method, other )
-		else
-			raise NoCollisionHandler
-		end
+		send( collide_method, other )
 	end
 end
