3e194b09f236586c95223152dc8228175114c5a1
John Croisant <jacius@gmail.com>
2007-07-08 20:13:23 +0000

    Add collision detection and hull visualization.

--- samples/glsprite.rb
+++ samples/glsprite.rb
@@ -19,15 +19,18 @@ def main()
 	panda = GLImageSprite.new {
 		@surface = Rubygame::Surface.load_image('big_panda.png')
 		@has_alpha = true
+		@pos = Point[WIDTH/2, HEIGHT/2]
+		@angle = 0.4
 		setup_texture()
 	}
 
 	class << panda
 		def update( time )
-			@t += time
-			@angle = 20 * Math::sin(@t / 300.0)
-			@scale = Vector2[1.0 + 0.05*Math::sin(@t/85.0),
-			                 1.0 + 0.05*Math::cos(@t/83.0)]
+#			@t += time
+#			@angle = 0.4 * Math::sin(@t / 300.0)
+#			@scale = Vector2[1.0 + 0.05*Math::sin(@t/85.0),
+#			                 1.0 + 0.05*Math::cos(@t/83.0)]
+			super
 		end
 	end
 
@@ -36,11 +39,15 @@ def main()
 		setup_texture()
 		@pos = Vector2[300,200]
 		@depth = -0.1
+		@angle = -0.2
 	}
 
 	group = GLGroup.new {
 		add_children(panda,ruby)
 	}
+
+	glEnable(GL_LINE_SMOOTH)
+	glLineWidth(3)
 
 	catch(:rubygame_quit) do
 		loop do
@@ -66,6 +73,20 @@ def main()
 
 			# redraw everything
 			view.clear()
+
+			glEnable(GL_TEXTURE_2D)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+			glColor([255,255,255])
+			group.draw()
+
+			if panda.shape.collide ruby.shape
+				glColor([255,0,0])
+			else
+				glColor([255,255,255])
+			end
+
+			glDisable(GL_TEXTURE_2D)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
 			group.draw()
 
 			scene.refresh()
