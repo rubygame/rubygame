226791346089a1064742fb97d409223cda4ff3fc
John Croisant <jacius@gmail.com>
2007-07-23 18:22:50 +0000

    Transitioning to Camera/Scene/Objects model.

--- samples/glsprite.rb
+++ samples/glsprite.rb
@@ -11,7 +11,7 @@ HEIGHT = 480
 def main()
 	Rubygame.init()
 	scene = Scene.new([WIDTH,HEIGHT])
-	view = View.new([WIDTH,HEIGHT])
+	scene.make_default_camera
 
 	queue = Rubygame::EventQueue.new()
 	clock = Rubygame::Clock.new { |c| c.target_framerate = 60 }
@@ -21,13 +21,14 @@ def main()
 		@has_alpha = true
 		@pos = Point[WIDTH/2, HEIGHT/2]
 		@angle = 0.4
+#		@scale = Vector2[0.5,0.5]
 		setup_texture()
 	}
 
 	class << panda
 		def update( time )
-#			@t += time
-#			@angle = 0.4 * Math::sin(@t / 300.0)
+			@t += time
+			@angle = 0.4 * Math::sin(@t / 300.0)
 #			@scale = Vector2[1.0 + 0.05*Math::sin(@t/85.0),
 #			                 1.0 + 0.05*Math::cos(@t/83.0)]
 			super
@@ -37,17 +38,15 @@ def main()
 	ruby = GLImageSprite.new {
 		@surface = Rubygame::Surface.load_image('ruby.png')
 		setup_texture()
-		@pos = Vector2[300,200]
+		@pos = Vector2[100,300]
 		@depth = -0.1
 		@angle = -0.2
 	}
+	
+	scene.objects.add_children(panda,ruby)
 
-	group = GLGroup.new {
-		add_children(panda,ruby)
-	}
-
-	glEnable(GL_LINE_SMOOTH)
-	glLineWidth(3)
+# 	glEnable(GL_LINE_SMOOTH)
+# 	glLineWidth(3)
 
 	catch(:rubygame_quit) do
 		loop do
@@ -55,6 +54,8 @@ def main()
 				case event
 				when Rubygame::MouseMotionEvent
 					panda.pos = Vector2[event.pos[0], HEIGHT - event.pos[1]]
+				when Rubygame::MouseDownEvent
+					ruby.pos = Vector2[event.pos[0], HEIGHT - event.pos[1]]
 				when Rubygame::KeyDownEvent
 					case event.key
 					when Rubygame::K_ESCAPE
@@ -69,25 +70,27 @@ def main()
 
 			# update everything
 			time = clock.tick
-			group.update(time)
+			scene.update(time)
 
 			# redraw everything
-			view.clear()
 
-			glEnable(GL_TEXTURE_2D)
-			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-			glColor([255,255,255])
-			group.draw()
+#  			glEnable(GL_TEXTURE_2D)
+#  			glEnable(GL_DEPTH_TEST)
+#  			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+#  			glColor([255,255,255])
 
-			if panda.shape.collide ruby.shape
-				glColor([255,0,0])
-			else
-				glColor([255,255,255])
-			end
-
-			glDisable(GL_TEXTURE_2D)
-			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-			group.draw()
+			scene.draw
+			
+# 			if panda.shape.collide ruby.shape
+# 				glColor([255,0,0])
+# 			else
+# 				glColor([255,255,255])
+# 			end
+#
+# 			glDisable(GL_TEXTURE_2D)
+# 			glDisable(GL_DEPTH_TEST)
+# 			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+# 			group.draw()
 
 			scene.refresh()
 
