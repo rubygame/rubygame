c2d177c3002ad6e851fbec7a1998387dd5323728
John Croisant <jacius@gmail.com>
2007-11-09 22:26:16 +0000

    Added new implementations of Point2 and Vector2.

---
+++ lib/rubygame/gl/point2.rb
@@ -0,0 +1,52 @@
+require 'rubygame/gl/vector2'
+require 'rubygame/gl/transform2'
+
+class Point2
+	attr_reader :x, :y
+	
+	class << self
+		def []( x, y )
+			self.new( x, y )
+		end
+		
+		def ify( *pairs )
+			pairs.collect { |pair| self.class.new(*pair) }
+		end
+	end
+	
+	def initialize( x, y )
+		@x, @y = x, y
+	end
+	
+	def +( vector )
+		self.class.new( @x + vector.x,  @y + vector.y )
+	end
+	
+	def -( other )
+		   Vector2.new( @x - other.x,   @y - other.y  )
+	end
+	
+	def projected_onto( v )
+		self.class.new(  *(v * v.dot(self.to_v) * (1/v.magnitude**2) )  )
+	end
+	
+	def to_ary
+		[@x, @y]
+	end
+	
+	def to_p
+		self
+	end
+	
+	def to_s
+		"#{self.class.name}[#{x}, #{y}]"
+	end
+	
+	def to_v
+		Vector2.new( @x, @y )
+	end
+	
+	def transformed( description = {} )
+		Transform2.new( description ) * self
+	end
+end
---
+++ lib/rubygame/gl/vector2.rb
@@ -0,0 +1,80 @@
+require 'rubygame/gl/point2'
+require 'rubygame/gl/transform2'
+
+class Vector2
+	attr_reader :x, :y
+	
+	class << self
+		def []( x, y )
+			self.new( x, y )
+		end
+		
+		def ify( *pairs )
+			pairs.collect { |pair| self.class.new(*pair) }
+		end
+	end
+	
+	def initialize( x, y )
+		@x, @y = x, y
+	end
+	
+	def +( vector )
+		self.class.new( @x + vector.x,  @y + vector.y )
+	end
+	
+	def -( other )
+		self.class.new( @x - other.x,   @y - other.y  )
+	end
+	
+	def angle
+		Math.atan2( @y, @x )
+	end
+	
+	def angle_with( other )
+		Math.acos( self.udot(other) )
+	end
+	
+	def dot( other )
+		(@x * other.x) + (@y * other.y)
+	end
+	
+	def magnitude
+		Math.hypot( @x, @y )
+	end
+	
+	def perp
+		self.class.new( -@y, @x )
+	end
+	
+	def projected_onto( v )
+		self.class.new(  *(v * v.dot(self) * (1/v.magnitude**2) )  )
+	end
+	
+	def to_ary
+		[@x, @y]
+	end
+	
+	def to_p
+		Point2.new( @x, @y)
+	end
+	
+	def to_s
+		"#{self.class.name}[#{x}, #{y}]"
+	end
+	
+	def to_v
+		self
+	end
+	
+	def transformed( description = {} )
+		Transform2.new( description ) * self
+	end
+	
+	def udot( other )
+		self.unit.dot( other.unit )
+	end
+
+	def unit
+		self * (1/magnitude())
+	end
+end
