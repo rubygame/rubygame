423eaeebc6441aae16e09c03d95813683b04ca78
John Croisant <jacius@gmail.com>
2007-11-10 18:23:57 +0000

    Added first draft RSpec specification for Matrix3.

---
+++ test/matrix3_spec.rb
@@ -0,0 +1,60 @@
+require 'rubygame/gl/matrix3'
+
+include Math
+
+describe Matrix3 do
+	before :each do
+		@identity = Matrix3[[1,0,0],[0,1,0],[0,0,1]]
+		@shear = Matrix3[[1,8,0],[2,1,0],[0,0,1]]
+		@scale = Matrix3[[4,0,0],[0,3,0],[0,0,1]]
+		@rotate = Matrix3[[cos(PI/2),-sin(PI/2),0],[sin(PI/2),cos(PI/2),0],[0,0,1]]
+		@translate = Matrix3[[1,0,10],[0,1,15],[0,0,1]]
+		@sample = Matrix3[[1,2,3],[4,5,6],[7,8,9]]
+	end
+
+	it "should have a 'new' constructor" do 
+		Matrix3.new([1,2,3],[4,5,6],[7,8,9]).should == @sample
+	end
+	
+	it "should have a [] constructor" do 
+		Matrix3[[1,2,3],[4,5,6],[7,8,9]].to_ary.should == @sample
+	end
+	
+	it "should have an 'identity' constructor" do 
+		Matrix3.identity.should == @identity
+	end
+	
+	it "should have a 'shear' constructor" do 
+		Matrix3.shear(8,2).should == @shear
+	end
+	
+	it "should have a 'scale' constructor" do 
+		Matrix3.scale(4,3).should == @scale
+	end
+	
+	it "should have a 'rotate' constructor" do 
+		Matrix3.rotate(PI/2).should == @rotate
+	end
+	
+	it "should have a 'translate' constructor" do 
+		Matrix3.translate(10,15).should == @translate
+	end
+	
+	it "should have an equality (==) operator" do 
+		@identity.should == @identity
+	end
+	
+	it "should support matrix multiplication with other Matrix3's" do 
+		result = @translate * @sample
+		result.should be_instance_of(Matrix3)
+		result.should == Matrix3[[71,82,93],[109,125,141],[7,8,9]]
+	end
+	
+	it "should be convertible to nested Arrays" do 
+		@sample.to_ary.should == [[1,2,3],[4,5,6],[7,8,9]]
+	end
+	
+	it "should have a #to_m method to 'convert' to Matrix3" do 
+		@sample.to_m.should == @sample
+	end
+end
