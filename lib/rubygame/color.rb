96992a4adc23a6c4d46b7df8935dd520cfecec29
John Croisant <jacius@gmail.com>
2007-12-18 17:16:56 +0000

    Color module uses GLOBAL palette.

--- lib/rubygame/color.rb
+++ lib/rubygame/color.rb
@@ -3,67 +3,24 @@ require 'rubygame/color_models/rgb'
 require 'rubygame/color_models/hsv'
 require 'rubygame/color_models/hsl'
 
+require 'rubygame/color_tables/palette'
 require 'rubygame/color_tables/x11'
 require 'rubygame/color_tables/css'
 
 module Rubygame
 	module Color
-		
-		# Look up a color from the color table.
-		# 
-		# The name is made uppercase and spaces become uppercase,
-		# to convert it to a module constant.
-		# 
-		# Example:
-		# 
-		#     # All do the same thing
-		#     Color::ALICE_BLUE
-		#     Color[ :alice_blue  ]
-		#     Color[ "alice blue" ]
-		# 
-		def self.[]( name )
-			self.const_get( name.to_s.gsub(' ','_').upcase.intern )
-		rescue NameError
-			raise NameError, "unknown color name `#{name}'"
-		end
 
-		# Set a color in the color table.
-		# 
-		# The name is made uppercase and spaces become uppercase,
-		# to convert it to a module constant.
-		# 
-		# Example:
-		# 
-		#     # All do the same thing
-		#     Color::GRUE      = ColorRGB.new( [ 0.0, 1.0, 0.0 ] )
-		#     Color[ :grue  ]  = ColorRGB.new( [ 0.0, 1.0, 0.0 ] )
-		#     Color[ "grue" ]  = ColorRGB.new( [ 0.0, 1.0, 0.0 ] )
-		# 
-		def self.[]=( name, color )
-			self.const_set( name.to_s.gsub(' ','_').upcase.intern, color )
+		GLOBAL = Palette.new()
+		GLOBAL.include CSS
+		
+		def self.[]( name )
+			GLOBAL[name]
 		end
 		
-		# call-seq:
-		#   import( hash )
-		#   import( module )
-		# 
-		# Include the colors defined in the Hash or Module in the
-		# Color namespace.
-		# 
-		def self.import( hashmod )
-			case hashmod
-			when Hash
-				hashmod.each_pair do | name, color |
-					self[name] = color
-				end
-			when Module
-				include hashmod
-			else
-				raise ArgumentError,
-				      "wrong type (got #{hashmod.class}, wanted Hash or Module)"
-			end
+		def self.[]=( name, color )
+			GLOBAL[name] = color
 		end
-			
+		
 	end
 end
 
