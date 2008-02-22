fb917fc699a9ac1a7d7e3bbcee8766f56b51a8e4
John Croisant <jacius@gmail.com>
2008-02-22 03:17:53 +0000

    Removed unnecessary restriction that Hook#initialize block must set trigger and action; they can be set later.

--- lib/rubygame/event_hook.rb
+++ lib/rubygame/event_hook.rb
@@ -8,17 +8,13 @@ class Hook
 		@owner, @trigger, @action = nil
 		@consumes = false
 		yield self if block_given?
-		unless (@trigger and @action)
-			# @owner is allowed to be nil, I think
-			raise( ArgumentError, "must set @trigger and @action")
-		end
 	end
 
 	def match?( event )
-		@trigger.match?( event )
+		@trigger.match?( event ) if @trigger
 	end
 	
 	def perform( event )
-		@action.perform( owner, event )
+		@action.perform( owner, event ) if @action
 	end
 end
