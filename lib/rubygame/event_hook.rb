b4d8ff243865ca2f368ddfd20964c66add5a1711
John Croisant <jacius@gmail.com>
2008-02-23 05:07:31 +0000

    Added license info and surrounding 'module Rubygame' to event_hook.rb, event_triggers.rb, and event_actions.rb

--- lib/rubygame/event_hook.rb
+++ lib/rubygame/event_hook.rb
@@ -1,6 +1,27 @@
+#--
+#	Rubygame -- Ruby code and bindings to SDL to facilitate game creation
+#	Copyright (C) 2004-2008  John Croisant
+#
+#	This library is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU Lesser General Public
+#	License as published by the Free Software Foundation; either
+#	version 2.1 of the License, or (at your option) any later version.
+#
+#	This library is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	Lesser General Public License for more details.
+#
+#	You should have received a copy of the GNU Lesser General Public
+#	License along with this library; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#++
+
 require 'rubygame/event_triggers'
 require 'rubygame/event_actions'
 
+module Rubygame 
+	
 class Hook
 	attr_accessor :owner, :trigger, :action, :consumes
 	
@@ -9,7 +30,7 @@ class Hook
 		@consumes = false
 		yield self if block_given?
 	end
-
+	
 	def match?( event )
 		@trigger.match?( event ) if @trigger
 	end
@@ -17,4 +38,6 @@ class Hook
 	def perform( event )
 		@action.perform( owner, event ) if @action
 	end
+end
+
 end
