5c22d6a415f8e0abb45779e519836b8661be16f5
John Croisant <jacius@gmail.com>
2008-02-28 06:58:47 +0000

    Added CollisionHandler.

---
+++ lib/rubygame/collision_handler.rb
@@ -0,0 +1,66 @@
+#--
+#	Rubygame -- Ruby code and bindings to SDL to facilitate game creation
+#	Copyright (C) 2004-2008  John Croisant
+#
+#	This library is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU Lesser General Public
+#	License as published by the Free Software Foundation; either
+#	version 2.1 of the License, or (at your option) any later version.
+#
+#	This library is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	Lesser General Public License for more details.
+#
+#	You should have received a copy of the GNU Lesser General Public
+#	License along with this library; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#++
+
+require 'rubygame/event'
+
+module Rubygame
+
+	class CollisionHandler
+		def initialize
+			@collisions = {}
+			@old_collisions = {}
+			@outbox = []
+		end
+		
+		def flush
+			_check_old_collisions
+			
+			out = @outbox
+			@outbox = []
+			return out
+		end
+		
+		def register_collision( a, b, contacts )
+			@collisions[ [a,b] ] = contacts
+			
+			if( @old_collisions[ [a,b] ] )
+				@outbox << CollisionEvent.new( a, b, contacts )
+			else
+				@outbox << CollisionStartEvent.new( a, b, contacts )
+			end
+		end
+		
+		private
+		
+		def _check_old_collisions
+			ended = @old_collisions.keys - @collisions.keys
+			
+			ended.each { |pair|
+				a, b = pair
+				contacts = @old_collisions[ pair ]
+				@outbox << CollisionEndEvent.new( a, b, contacts )
+			}
+			
+			@old_collisions = @collisions
+			@collisions = {}
+		end
+		
+	end
+
+end
