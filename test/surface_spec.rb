5d622d2518de426ffb68fbda61d598f4a9598db9
John Croisant <jacius@gmail.com>
2008-05-17 01:25:56 +0000

    Copied over Surface tests.

--- test/surface_spec.rb
+++ test/surface_spec.rb
@@ -3,82 +3,215 @@
 require 'rubygame'
 include Rubygame
 
-context "A Surface being created (with a Screen available)" do
-  setup do
+samples_dir = File.join( File.dirname(__FILE__), "..", "samples", "")
+test_dir = File.join( File.dirname(__FILE__), "" )
+
+test_image = test_dir + "image.png"
+not_image = test_dir + "short.ogg"
+panda = samples_dir + "panda.png"
+dne = test_dir + "does_not_exist.png"
+
+
+describe Surface, "(creation)" do
+  before(:each) do
+    Rubygame.init()
     @screen = Screen.new([100,100])
   end
 
-  teardown do
-    Rubygame.quit
+  after(:each) do
+    Rubygame.quit()
   end
 
-  specify "should raise TypeError when #new size is not an Array" do
+  it "should raise TypeError when #new size is not an Array" do
     lambda {
       Surface.new("not an array")
     }.should raise_error(TypeError)
   end
 
-  specify "should raise ArgumentError when #new size is an Array of non-Numerics" do
+  it "should raise ArgumentError when #new size is an Array of non-Numerics" do
     lambda {
       Surface.new(["not", "numeric", "members"])
     }.should raise_error(TypeError)
   end
 
-  specify "should raise ArgumentError when #new size is too short" do
+  it "should raise ArgumentError when #new size is too short" do
     lambda {
       Surface.new([1])
     }.should raise_error(ArgumentError)
   end
 end
 
-# Maybe an overly-broad context?
-context "An existing Surface" do
-  setup do
+
+
+describe Surface, "(loading)" do
+  before :each do
+    unless( Rubygame::VERSIONS[:sdl_image] )
+      raise "Can't test image loading, no SDL_image installed."
+    end
+  end
+
+  it "should load image to a new Surface" do
+    surface = Surface.load( test_image )
+  end
+
+  it "should raise an error if file is not an image" do
+    lambda{ Surface.load( not_image ) }.should raise_error( SDLError )
+  end
+
+  it "should raise an error if file doesn't exist" do
+    lambda{ Surface.load( dne ) }.should raise_error( SDLError )
+  end
+end
+
+
+describe Surface, "(loading from string)" do
+  before :each do
+    unless( Rubygame::VERSIONS[:sdl_image] )
+      raise "Can't test image loading, no SDL_image installed."
+    end
+
+    @data = "\x42\x4d\x3a\x00\x00\x00\x00\x00"+
+            "\x00\x00\x36\x00\x00\x00\x28\x00"+
+            "\x00\x00\x01\x00\x00\x00\x01\x00"+
+            "\x00\x00\x01\x00\x18\x00\x00\x00"+
+            "\x00\x00\x04\x00\x00\x00\x13\x0b"+
+            "\x00\x00\x13\x0b\x00\x00\x00\x00"+
+            "\x00\x00\x00\x00\x00\x00\x00\x00"+
+            "\xff\x00"
+  end
+  
+  it "should be able to load from string" do
+    surf = Surface.load_from_string(@data)
+    surf.get_at(0,0).should == [255,0,0,255]
+  end
+  
+  it "should be able to load from string (typed)" do
+    surf = Surface.load_from_string(@data,"BMP")
+    surf.get_at(0,0).should == [255,0,0,255]
+  end
+end
+
+
+describe Surface, "(named resource)" do
+  before :each do
+    Surface.autoload_dirs = [samples_dir]
+  end
+
+  after :each do
+    Surface.autoload_dirs = []
+    Surface.instance_eval { @resources = {} }
+  end
+
+  it "should include NamedResource" do
+    Surface.included_modules.should include(NamedResource)
+  end
+
+  it "should respond to :[]" do
+    Surface.should respond_to(:[])
+  end
+
+  it "should respond to :[]=" do
+    Surface.should respond_to(:[]=)
+  end
+
+  it "should allow setting resources" do
+    s = Surface.load(panda)
+    Surface["panda"] = s
+    Surface["panda"].should == s
+  end
+
+  it "should reject non-Surface resources" do
+    lambda { Surface["foo"] = "bar" }.should raise_error(TypeError)
+  end
+
+  it "should autoload images as Surface instances" do
+    unless( Rubygame::VERSIONS[:sdl_image] )
+      raise "Can't test image loading, no SDL_image installed."
+    end
+
+    Surface["panda.png"].should be_instance_of(Surface)
+  end
+
+  it "should return nil for nonexisting files" do
+    unless( Rubygame::VERSIONS[:sdl_image] )
+      raise "Can't test image loading, no SDL_image installed."
+    end
+
+    Surface["foobar.png"].should be_nil
+  end
+
+  it "should set names of autoload Surfaces" do
+    unless( Rubygame::VERSIONS[:sdl_image] )
+      raise "Can't test image loading, no SDL_image installed."
+    end
+
+    Surface["panda.png"].name.should == "panda.png"
+  end
+end
+
+
+
+describe Surface, "(blit)" do
+  before(:each) do
+    Rubygame.init()
     @screen = Screen.new([100,100])
     @surface = Surface.new([100,100])
   end
 
-  teardown do
+  after(:each) do
     Rubygame.quit
   end
 
-  specify "should raise TypeError when #blit target is not a Surface" do
+  it "should raise TypeError when #blit target is not a Surface" do
     lambda {
       @surface.blit("not a surface", [0,0])
     }.should raise_error(TypeError)
   end
 
-  specify "should raise TypeError when #blit dest is not an Array" do
+  it "should raise TypeError when #blit dest is not an Array" do
     lambda {
       @surface.blit(@screen, "foo")
     }.should raise_error(TypeError)
   end
 
-  specify "should raise TypeError when #blit src is not an Array" do
+  it "should raise TypeError when #blit src is not an Array" do
     lambda { 
       @surface.blit(@screen, [0,0], "foo")
     }.should raise_error(TypeError)
   end
+end
 
-  specify "should raise TypeError when #fill color is not an Array" do
+
+
+describe Surface, "(fill)" do 
+  before(:each) do
+    Rubygame.init()
+    @surface = Surface.new([100,100])
+  end
+
+  after(:each) do
+    Rubygame.quit
+  end
+
+  it "should raise TypeError when #fill color is not an Array" do
     lambda {
-      @surface.fill("not_an_array")
+      @surface.fill(nil)
     }.should raise_error(TypeError)
   end
 
-  specify "should raise TypeError when #fill color is an Array of non-Numerics" do
+  it "should raise TypeError when #fill color is an Array of non-Numerics" do
     lambda {
       @surface.fill(["non", "numeric", "members"])
     }.should raise_error(TypeError)
   end
 
-  specify "should raise ArgumentError when #fill color is too short" do
+  it "should raise ArgumentError when #fill color is too short" do
     lambda {
       @surface.fill([0xff, 0xff])
     }.should raise_error(TypeError)
   end
 
-  specify "should raise TypeError when #fill color is not an Array" do
+  it "should raise TypeError when #fill rect is not an Array" do
     lambda {
       @surface.fill([0xff, 0xff, 0xff], "not_an_array")
     }.should raise_error(TypeError)
