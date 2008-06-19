b5cc33454ad00f9e066f20ddf1b4921129dca9b3
John Croisant <jacius@gmail.com>
2008-06-19 15:17:55 -0500

    Added bare specs for KeyPressed and KeyReleased.

---
+++ spec/keyboard_events_spec.rb
@@ -0,0 +1,42 @@
+
+# Prefer local library over installed version.
+$:.unshift( File.join( File.dirname(__FILE__), "..", "lib" ) )
+$:.unshift( File.join( File.dirname(__FILE__), "..", "ext", "rubygame" ) )
+
+require 'rubygame'
+include Rubygame::Events
+
+
+
+describe "a keyboard event", :shared => true do
+  
+  it "should have a key symbol"
+
+  it "should have an array of modifiers"
+ 
+end
+
+
+
+describe "KeyPressed Event" do
+
+  before :each do
+    @event = KeyPressed.new( :a, [:shift], "A" )
+  end
+
+  it_should_behave_like "a keyboard event"
+
+  it "should have a utf8 string"
+
+end
+
+
+describe "KeyReleased Event" do
+  
+  before :each do
+    @event = KeyReleased.new( :a, [:shift] )
+  end
+
+  it_should_behave_like "a keyboard event"
+
+end
