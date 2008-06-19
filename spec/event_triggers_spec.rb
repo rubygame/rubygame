f18a438d87ac72287acb122beafbd4ebeaaa0425
John Croisant <jacius@gmail.com>
2008-06-19 01:29:06 -0500

    Cleaned up to get all specs passing by getting rid of references to TickEvent, which has not been imported yet.

--- spec/event_triggers_spec.rb
+++ spec/event_triggers_spec.rb
@@ -7,6 +7,8 @@ require 'rubygame'
 include Rubygame
 
 
+FakeEvent = Struct.new(:a)
+
 
 describe "an event trigger", :shared => true do 
 	
@@ -33,19 +35,19 @@ end
 describe AllTrigger do 
 	
 	before :each do 
-		trigger1 = TickTrigger.new
-		trigger2 = AttrTrigger.new( :seconds => 0.5 )
+		trigger1 = InstanceOfTrigger.new(FakeEvent)
+		trigger2 = AttrTrigger.new( :a => 0.5 )
 		@trigger = AllTrigger.new( trigger1, trigger2 )
 	end
 	
 	it_should_behave_like "an event trigger"
 
 	it "should match if all included triggers match" do 
-		@trigger.match?( TickEvent.new(0.5) ).should be_true
+		@trigger.match?( FakeEvent.new(0.5) ).should be_true
 	end
 	
 	it "should not match if any of the included triggers does not match" do 
-		@trigger.match?( TickEvent.new(0.1) ).should be_false
+		@trigger.match?( FakeEvent.new(0.1) ).should be_false
 	end
 	
 	it "should not match if none of the included triggers matches" do 
@@ -59,19 +61,19 @@ end
 describe AnyTrigger do 
 	
 	before :each do 
-		trigger1 = TickTrigger.new
-		trigger2 = AttrTrigger.new( :seconds => 0.5 )
+		trigger1 = InstanceOfTrigger.new(FakeEvent)
+		trigger2 = AttrTrigger.new( :a => 0.5 )
 		@trigger = AnyTrigger.new( trigger1, trigger2 )
 	end
 	
 	it_should_behave_like "an event trigger"
 
 	it "should match if all included triggers match" do 
-		@trigger.match?( TickEvent.new(0.5) ).should be_true
+		@trigger.match?( FakeEvent.new(0.5) ).should be_true
 	end
 	
 	it "should match if any of the included triggers match" do 
-		@trigger.match?( TickEvent.new(0.1) ).should be_true
+		@trigger.match?( FakeEvent.new(0.1) ).should be_true
 	end
 
 	it "should not match if none of the included triggers matches" do 
@@ -203,23 +205,23 @@ end
 
 
 
-describe TickTrigger do 
-	
-	before :each do 
-		@trigger = TickTrigger.new
-	end
-	
-	it_should_behave_like "an event trigger"
-
-	it "should match if the event is an instance of TickEvent" do 
-		@trigger.match?( TickEvent.new(0.1) ).should be_true
-	end
-
-	it "should not match if the event is not an instance of TickEvent" do 
-		@trigger.match?( :foo ).should be_false
-	end
-	
-end
+# describe TickTrigger do 
+#
+# 	before :each do 
+# 		@trigger = TickTrigger.new
+# 	end
+#
+# 	it_should_behave_like "an event trigger"
+#
+# 	it "should match if the event is an instance of TickEvent" do 
+# 		@trigger.match?( TickEvent.new(0.1) ).should be_true
+# 	end
+#
+# 	it "should not match if the event is not an instance of TickEvent" do 
+# 		@trigger.match?( :foo ).should be_false
+# 	end
+#	
+# end
 
 
 
@@ -235,7 +237,7 @@ describe YesTrigger do
 		@trigger.match?( true ).should be_true
 		@trigger.match?( false ).should be_true
 		@trigger.match?( nil ).should be_true
-		@trigger.match?( TickEvent.new(0.1) ).should be_true
+		@trigger.match?( FakeEvent.new(0.1) ).should be_true
 		@trigger.match?( :foo ).should be_true
 		@trigger.match?( -4 ).should be_true
 	end
