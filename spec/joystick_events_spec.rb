77f76c1b4d9f5e1484e4027d3c97a40dff2e5fc6
John Croisant <jacius@gmail.com>
2008-07-24 19:53:13 -0500

    Added commented-out specs for "a joystick button event".

--- spec/joystick_events_spec.rb
+++ spec/joystick_events_spec.rb
@@ -192,6 +192,24 @@ end
 
 
 describe "a joystick button event", :shared => true do
+
+#   it "should have a button number" do
+#     @event.should respond_to(:button)
+#   end
+
+#   it "should set button from initialize arg" do
+#     make_event(:button => 1).button.should == 1
+#   end
+
+#   it "should accept only non-negative integers for button" do
+#     [-1, 1.2, :foo, "red", [], {}].each do |thing|
+#       lambda { make_event(:button => thing) }.should raise_error
+#     end
+#   end
+
+#   it "button should be read-only" do
+#     @event.should_not respond_to(:button=)
+#   end
   
 end
 
