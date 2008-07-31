fc3ae2baacde8b0c2fe6088d5c0b6390bd28f6ea
John Croisant <jacius@gmail.com>
2008-07-31 18:45:31 -0500

    EventQueue #each should yield each event in the queue in order.

---
+++ spec/event_queue_spec.rb
@@ -0,0 +1,23 @@
+
+# Prefer local library over installed version.
+$:.unshift( File.join( File.dirname(__FILE__), "..", "lib" ) )
+$:.unshift( File.join( File.dirname(__FILE__), "..", "ext", "rubygame" ) )
+
+require 'rubygame'
+include Rubygame
+
+
+describe EventQueue do
+  
+  before :each do
+    @queue = EventQueue.new
+  end
+
+  it "#each should yield each event in the queue in order" do 
+    collect = []
+    @queue.push( [1,2,3] )
+    @queue.each { |e| collect << e}
+    collect.should == [1,2,3]
+  end
+
+end
