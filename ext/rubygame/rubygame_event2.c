8f2dcae66dc00dcf17b9f1cdae7228966446746b
John Croisant <jacius@gmail.com>
2008-07-31 02:35:29 -0500

    Can now fetch JoyHatMoved events. (Pending testing.)

--- ext/rubygame/rubygame_event2.c
+++ ext/rubygame/rubygame_event2.c
@@ -554,6 +554,9 @@ VALUE rg_convert_sdlevent2( SDL_Event ev )
     case SDL_JOYBUTTONUP:
       return rg_convert_joystickbuttonevent(ev);
 
+    case SDL_JOYHATMOTION:
+      return rg_convert_joyhatevent(ev);
+
     case SDL_KEYDOWN:
     case SDL_KEYUP:
       return rg_convert_keyboardevent(ev);
