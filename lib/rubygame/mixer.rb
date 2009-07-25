aa71799386b4014a382c4d55286522b70a3de655
John Croisant <jacius@gmail.com>
2009-07-24 21:44:20 -0500

    Added mixer.rb.
    Require ruby-sdl-ffi/mixer, defines version, and
    loads sound.rb and music.rb.

---
+++ lib/rubygame/mixer.rb
@@ -0,0 +1,30 @@
+#--
+#	Rubygame -- Ruby code and bindings to SDL to facilitate game creation
+#	Copyright (C) 2004-2009  John Croisant
+#
+#	This library is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU Lesser General Public
+#	License as published by the Free Software Foundation; either
+#	version 2.1 of the License, or (at your option) any later version.
+#
+#	This library is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	Lesser General Public License for more details.
+#
+#	You should have received a copy of the GNU Lesser General Public
+#	License along with this library; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#++
+
+
+
+require "ruby-sdl-ffi/mixer"
+
+Rubygame::VERSIONS[:sdl_mixer] = SDL::Mixer.Linked_Version.to_ary
+
+
+# Load sound.rb and music.rb
+%w{ sound music }.each do |f|
+  require File.join( File.dirname(__FILE__), f )
+end
