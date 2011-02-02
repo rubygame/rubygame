920c96e92e2769312db1d2c4eaa8dab21f18d17b
Bart Leusink <bartleusink@gmail.com>
2011-02-02 17:10:47 +0100

    Fixed specs on ruby 1.8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
 
 # Prefer to load from ../lib before the system paths.
-$:.unshift File.absolute_path(File.join(File.dirname(__FILE__),"..","lib"))
+$:.unshift File.expand_path(File.join(File.dirname(__FILE__),"..","lib"))
