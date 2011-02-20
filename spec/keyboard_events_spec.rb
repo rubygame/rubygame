9d59f9baa91bdb756f666f5620bd0a19c1b45736
John Croisant <jacius@gmail.com>
2011-02-19 18:29:57 -0600

    Added specs for Rubygame.pressed_keys.

--- spec/keyboard_events_spec.rb
+++ spec/keyboard_events_spec.rb
@@ -69,3 +69,50 @@ describe KeyReleased do
   it_should_behave_like "a keyboard event"
 
 end
+
+
+
+describe "Rubygame.pressed_keys" do
+
+  # Helper method to make an Array like would be returned from
+  # SDL.GetKeyState. If you pass SDL::K_* constants to this method,
+  # they will be marked as "pressed" in the array.
+  # 
+  def mock_keys( *pressed )
+    keys = Array.new(SDL::K_LAST, 0)
+    pressed.each{ |key| keys[key] = 1 }
+    keys
+  end
+
+  it "should query SDL.GetKeyState" do
+    SDL.should_receive(:GetKeyState).and_return( mock_keys() )
+    Rubygame.pressed_keys
+  end
+
+  it "should return an empty hash when no keys are pressed" do
+    SDL.stub(:GetKeyState){ mock_keys() }
+    Rubygame.pressed_keys.should == {}
+  end
+
+  it "should have a pair for every pressed key" do
+    SDL.stub(:GetKeyState){ mock_keys( SDL::K_a, SDL::K_LCTRL ) }
+    keys = Rubygame.pressed_keys
+    keys.should have_key( :a )
+    keys.should have_key( :left_ctrl )
+  end
+
+  it "should have a value of true for every pressed key" do
+    SDL.stub(:GetKeyState){ mock_keys( SDL::K_a, SDL::K_LCTRL ) }
+    keys = Rubygame.pressed_keys
+    keys[:a].should be_true
+    keys[:left_ctrl].should be_true
+  end
+
+  it "should have a falsey value for non-pressed keys" do
+    SDL.stub(:GetKeyState){ mock_keys( SDL::K_a, SDL::K_LCTRL ) }
+    keys = Rubygame.pressed_keys
+    keys[:b].should satisfy{ |v|  not v }
+    keys[:left_shift].should satisfy{ |v|  not v }
+  end
+
+end
